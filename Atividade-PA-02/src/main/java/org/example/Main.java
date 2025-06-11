687d6d2629584d760e99918f7d64fce52e4c7d8b
BrunoAbrahim <brunoabval@gmail.com>
2025-06-10 21:57:49 -0300

    teste de commit no git

--- Atividade-PA-02/src/main/java/org/example/Main.java
+++ Atividade-PA-02/src/main/java/org/example/Main.java
@@ -15,5 +15,6 @@ public class Main {
             System.out.println("Autonomia: " + veiculo.calcularAutonomia() + " km");
             System.out.println("---------------------------");
         }
+
     }
 }
